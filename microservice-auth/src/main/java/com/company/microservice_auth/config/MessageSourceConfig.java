0897f2cdddafc04d666f852552909a624b653a2a
Luis Ore <luisore24@gmail.com>
2025-09-07 13:07:26 -0500

    Add MessageSourceConfig to get from messages.yml

---
+++ microservice-auth/src/main/java/com/company/microservice_auth/config/MessageSourceConfig.java
@@ -0,0 +1,19 @@
+package com.company.microservice_auth.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class MessageSourceConfig {
+
+    @Bean
+    public MessageSource messageSourceConfig(){
+        ReloadableResourceBundleMessageSource messageSourceConfig = new ReloadableResourceBundleMessageSource();
+        messageSourceConfig.setBasenames("classpath:messages");
+        messageSourceConfig.setDefaultEncoding("UTF-8");
+        return messageSourceConfig;
+    }
+
+}
